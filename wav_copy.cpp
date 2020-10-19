89eb83b79bc1a467f8153431323ab0285539a7b8
Inês Justo <inesjusto@ua.pt>
2020-10-19 18:59:25 +0100

    wav copy done

--- wav_copy.cpp
+++ wav_copy.cpp
@@ -3,63 +3,49 @@
 //
 
 #include    <cstdio>
-#include    <cstring>
+#include    <iostream>
 #include    <sndfile.hh>
 
-#define        BUFFER_LEN        100000024
+static void copyFile(const char *sourceFName, const char *destFName) {
 
-static void createDestFile(const SndfileHandle *sourceFile, const char *fname) {
+    SndfileHandle sourceFile = SndfileHandle(sourceFName, SFM_READ);
 
-    const int64_t size = sourceFile->frames();
-    short buffer[size];
-    SndfileHandle destFile  = SndfileHandle(fname, SFM_WRITE, sourceFile->format(), sourceFile->channels(),
-            sourceFile->samplerate());
-
-    memset(buffer, 0, sizeof(buffer));
-
-    destFile.write(buffer, size);
-
+    int64_t srcFileFrames = sourceFile.frames();
+    int srcFileChannels = sourceFile.channels();
+    short buffer[srcFileFrames*srcFileChannels];
+    sourceFile.read(buffer, srcFileFrames*srcFileChannels);
     puts("");
+
     /*
-    **	The SndfileHandle object will automatically close the file and
-    **	release all allocated memory when the object goes out of scope.
-    **	This is the Resource Acquisition Is Initailization idom.
-    **	See : http://en.wikipedia.org/wiki/Resource_Acquisition_Is_Initialization
-    */
-} /* create_file */
-
-static void copyFile(const char *sourcefname, const char *destfname) {
-
-    SndfileHandle sourceFile = SndfileHandle(sourcefname, SFM_READ);
-
-    const int64_t size = sourceFile.frames();
-    short buffer[size];
-
     printf("Opened file '%s'\n", sourcefname);
     printf("    Sample rate : %d\n", sourceFile.samplerate());
+    printf("    Frames : %d\n", (int)sourceFile.frames());
     printf("    Channels    : %d\n", sourceFile.channels());
+    */
 
-    createDestFile(&sourceFile, destfname);
-
-    sourceFile.read(buffer, size);
-    SndfileHandle destFile = SndfileHandle(destfname, SFM_WRITE, sourceFile.format(), sourceFile.channels(),
+    /* create dest wav file */
+    SndfileHandle destFile  = SndfileHandle(destFName, SFM_WRITE, sourceFile.format(), srcFileChannels,
             sourceFile.samplerate());
-    destFile.write(buffer, size);
+    destFile.write(buffer, srcFileFrames*srcFileChannels);
 
     puts("");
 
     /* RAII takes care of destroying SndfileHandle object. */
-} /* read_file */
+}
 
-int main(void) {
-    const char *destfname = "copysample01.wav";
-    const char *sourcefpath = "../wav_files/sample01.wav";
+int main(int argc, char *argv[]) {
+    if(argc < 3){
+        puts("Not enough program arguments.\nArguments: [source file path] [destination file path]");
+    } else {
+        const char *sourceFPath = argv[argc-2];  // "../wav_files/sample01.wav"
+        const char *destFName = argv[argc-1];  // "copysample01.wav";
 
-    puts("\nSimple example showing usage of the C++ SndfileHandle object.\n");
+        puts("\nSimple example showing usage of the C++ SndfileHandle object.\n");
 
-    copyFile(sourcefpath, destfname);
+        copyFile(sourceFPath, destFName);
 
-    puts("Done.\n");
+        puts("Done.\n");
+    }
     return 0;
-} /* main */
+}
 
