87e2f9bff0d95ac499b455b0c99e9b0c36831660
Inês Justo <inesjusto@ua.pt>
2020-10-30 11:34:01 +0000

    entropy

--- audio/wavCopy.cpp
+++ audio/wavCopy.cpp
@@ -12,21 +12,6 @@
 
 #define		FRAMES_BUFFER_LEN		65536
 
-static void copyFile(const char *sourceFName, const char *destFName) {
-
-    SndfileHandle sourceFile = SndfileHandle(sourceFName, SFM_READ);
-    int srcFileChannels = sourceFile.channels();
-
-    SndfileHandle destFile  = SndfileHandle(destFName, SFM_WRITE, sourceFile.format(), srcFileChannels,
-                                            sourceFile.samplerate());
-
-    std::vector<short> buffer(FRAMES_BUFFER_LEN * srcFileChannels);
-    for(sf_count_t nFrames = sourceFile.readf(buffer.data(), FRAMES_BUFFER_LEN); nFrames != 0;
-            nFrames = sourceFile.readf(buffer.data(), FRAMES_BUFFER_LEN)){
-        destFile.writef(buffer.data(), nFrames);
-    }
-}
-
 int main(int argc, char *argv[]) {
 
     if(argc < 3){
@@ -34,9 +19,17 @@ int main(int argc, char *argv[]) {
         return 0;
     }
 
-    copyFile(argv[argc-2], argv[argc-1]);
+    SndfileHandle sourceFile = SndfileHandle(argv[argc-2], SFM_READ);
+    int srcFileChannels = sourceFile.channels();
 
-    puts("Done.\n");
+    SndfileHandle destFile  = SndfileHandle(argv[argc-1], SFM_WRITE, sourceFile.format(), srcFileChannels,
+                                                sourceFile.samplerate());
+
+    std::vector<short> buffer(FRAMES_BUFFER_LEN * srcFileChannels);
+    for(sf_count_t nFrames = sourceFile.readf(buffer.data(), FRAMES_BUFFER_LEN);
+            nFrames != 0; nFrames = sourceFile.readf(buffer.data(), FRAMES_BUFFER_LEN)){
+        destFile.writef(buffer.data(), nFrames);
+    }
 
     return 0;
 }
