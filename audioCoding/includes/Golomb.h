37e9f55c1c2ace5adeebe7ccae7a5f0a8f9b1c17
Inês Justo <inesjusto@ua.pt>
2020-11-08 22:10:59 +0000

    golomb first structure

--- audioCoding/includes/Golomb.h
+++ audioCoding/includes/Golomb.h
@@ -1,4 +1,43 @@
 #ifndef AUDIOCODING_GOLOMB_H
 #define AUDIOCODING_GOLOMB_H
 
+//!  Golomb
+/*!
+ *  This class is useful to encode and decode with the Golomb Code.
+ *  @author Inês Justo
+ *  @author ...
+*/
+class Golomb {
+
+private:
+    unsigned int m;
+    /* TODO: put here methods for unary and truncated binary codes handling */
+
+public:
+
+    //! Unsigned encode.
+    /*!
+     * @param n
+    */
+    void uEncode(unsigned int n); /* TODO: return type ?? */
+
+    //! Signed encode.
+    /*!
+     * @param n
+    */
+    void sEncode(int n); /* TODO: return type ?? */
+
+    //! Unsigned decode.
+    /*!
+     * @param n
+    */
+    unsigned uDecode(unsigned int q, unsigned int r);
+
+    //! Signed decode.
+    /*!
+     * @param n
+    */
+    int sDecode(unsigned int q, unsigned int r);
+}
+
 #endif //AUDIOCODING_GOLOMB_H
--- audioCoding/src/Golomb.cpp
+++ audioCoding/src/Golomb.cpp
@@ -1,2 +1,19 @@
-
 #include "../includes/Golomb.h"
+
+void Golomb::uEncode(unsigned int n) {
+    unsigned int q = n / this->m;
+    unsigned int r = n % m; /* <=> n-q*m  - TODO: what is more optimal */
+    /* TODO: invoke private method to encode unary and truncated binary codes*/
+}
+
+void Golomb::sEncode(int n) {
+
+}
+
+unsigned Golomb::uDecode(unsigned int q, unsigned int r) {
+
+}
+
+int Golomb::sDecode(unsigned int q, unsigned int r) {
+
+}
