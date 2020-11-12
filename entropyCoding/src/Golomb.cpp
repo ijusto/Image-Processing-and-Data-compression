a79b5730b726c99f5b95ab3312a4993215f79f34
Inês Justo <inesjusto@ua.pt>
2020-11-12 22:53:23 +0000

    encode

--- entropyCoding/src/Golomb.cpp
+++ entropyCoding/src/Golomb.cpp
@@ -1,5 +1,6 @@
 #include "../includes/Golomb.h"
 #include <cmath>
+#include <vector>
 
 Golomb::Golomb(unsigned int _m, Bitstream _bitStream): m(_m), bitStream(_bitStream){}
 
@@ -8,31 +9,11 @@ void Golomb::uEncode(unsigned int n) {
     unsigned int r = n % this.m; /* <=> n-q*m  - TODO: what is more optimal */
     unsigned char unary = this.encodeUnary(q);
     std::tuple<unsigned char, unsigned int> binaryRes = this.encodeTruncatedBinary(r);
-    unsigned char binary = get<0>(res);
-    unsigned int nBinBits = get<1>(res);
+    unsigned char binary = get<0>(binaryRes);
+    unsigned int nBinBits = get<1>(binaryRes);
 
-    /*
-     * bitStream.writeNBits(unary, q + 1);
-     * bitStream.writeNBits(binary, nBinBits);
-     *
-     * OR (if unsigned chars):
-     */
-
-    unsigned char code = unary | (binary>>(q+1));
-    // bitStream.writeNBits(code, q + 1 + nBinBits);
-
-    // number of bits from the truncated binary lost in the shift operation
-    int nLostBits = 8 - (q + 1 + nBinBits);
-    if(nLostBits){
-        unsigned char lostBits = 0x00;
-        unsigned char tempBin = binary;
-        for(int i = q; i >= 0; i--){
-            bool ret = tempBin & 1;
-            tempBin >>= 1;
-            lostBits = (lostBits | (ret<<(7-i)));
-        }
-        // bitStream.writeNBits(lostBits, nLostBits);
-    }
+    this.bitStream.writeNBits(unary, q + 1);
+    this.bitStream.writeNBits(&binary, nBinBits);
 }
 
 void Golomb::sEncode(int n) {
@@ -46,26 +27,27 @@ unsigned char Golomb::encodeUnary(unsigned int q) {
 
 std::tuple<unsigned char, unsigned int> Golomb::encodeTruncatedBinary(unsigned int r) {
     unsigned int b = (unsigned int) ceil(log2(this.m));
-    unsigned int nBits, codeNumber;
     /* Encode the first 2**b − m values of r using the first 2**b−m binary codewords of b−1 bits */
-    if(r < (pow(2,b) - this.m)){
-        codeNumber = r;
-        nBits = b - 1;
-    } else {
-        /* Encode the remainder values of r by coding the number r+2**b−m in binary codewords of b bits. */
-        codeNumber = r + pow(2,b) - this.m;
-        nBits = b;
-    }
+    unsigned int codeNumber = r;
+    unsigned int nBits = b - 1;
+    /* Encode the remainder values of r by coding the number r+2**b−m in binary codewords of b bits. */
+    if(!(r < (pow(2,b) - this.m)){ codeNumber += pow(2,b) - this.m; nBits += 1; }
 
     /* Conversion of decimal code number to binary*/
-    unsigned  char bin = 0x00;
+    std::vector<int> binInt;
     for(int i = 0; codeNumber > 0; codeNumber /= 2, i++) {
-        bin |= ((codeNumber % 2) << i);
+        binInt.push_back((codeNumber % 2));
+    }
+    unsigned char binChar[binInt.size()];
+    for (int i = 0; i < binInt.size(); i++){
+        if(binInt.at(i) == 0){
+            binChar[binInt.size() -1 - i] = '0';
+        } else{
+            binChar[binInt.size() -1 - i] = '1';
+        }
     }
 
-    /* Shift the truncated binary code to the most significant bits */
-    bin <<= (8 - nBits);
-    return std::make_tuple(bin, nBits);
+    return std::make_tuple(binChar, nBits);
 }
 
 unsigned int Golomb::uDecode() {
@@ -84,10 +66,7 @@ int Golomb::sDecode() {
 */
 unsigned int Golomb::decodeUnary() {
     unsigned int q = 0;
-
-    while(this.bitStream.readBit() == '0'){ // if readBit returns the bit or the boolean associated
-        q++;
-    }
+    while(this.bitStream.readBit() == '0'){ q++; }
     return q;
 }
 
