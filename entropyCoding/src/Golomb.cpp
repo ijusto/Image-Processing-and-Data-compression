624d9e1c5e3c7f83bc0efa0695b6b5b4e5185340
Inês Justo <inesjusto@ua.pt>
2020-11-13 14:38:51 +0000

    minor fix

--- entropyCoding/src/Golomb.cpp
+++ entropyCoding/src/Golomb.cpp
@@ -151,8 +151,6 @@ vector<int> Golomb::decode(vector<bool> encoded_n) {
         } else {
             n = (int) -(nMapped + 1)/2;
         }
-
-        std::cout << "decode number " << n << std::endl;
         numbers.push_back(n);
     }
 
