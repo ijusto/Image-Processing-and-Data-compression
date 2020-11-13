7f4929914ef34206f64ca73c94e7348c7ad3a57d
iotmaster <agostinho.pires.95gmail.com>
2020-11-13 13:51:53 +0000

    new test bitstream

--- entropyCoding/test/testBitStream.cpp
+++ entropyCoding/test/testBitStream.cpp
@@ -10,35 +10,27 @@
 using namespace std;
 
 int main(int argc, char* argv[]) {
-    if (argc < 4){
-        cout << "Usage: " << argv[0] << " src_path dst_path Nbits" << endl;
+    if (argc < 5){
+        cout << "Usage: " << argv[0] << " src_path dst_path Nbits number of times to read" << endl;
         cout << "src_path:\n\tpath to test_file_to_read" << endl;
         cout << "dst_path:\n\tpath to new_text_file" << endl;
         cout << "Nbits:\n\tnumber of bits" << endl;
+        cout << "Nbits:\n\tnumber of times to read" << endl;
         return 0;
     }
 
-    auto * BS = new BitStream(argv[argc-3]);
-    int NBITS = stoi(argv[argc-1]);
+    auto * BS = new BitStream(argv[argc-4]);
+    int NBITS = stoi(argv[argc-2]);
     unsigned char bits[NBITS];
 
     //construct read_N_bits
-    BS->readNbits(NBITS,bits);
-    cout << "Bits read = "<< bits << endl;
+    for (int i, i < stoi(argv[argc-1], i++ ){
+        BS->readNbits(NBITS,bits);
+        cout << "Bits read = "<< bits << endl;
+        BS -> writeNbits(NBITS,bits);
+    }
 
-    BS -> writeNbits(NBITS,bits);
-
-    BS->readNbits(NBITS,bits);
-    cout << "Bits read = "<< bits << endl;
-
-    BS -> writeNbits(NBITS,bits);
-
-    BS->readNbits(NBITS,bits);
-    cout << "Bits read = "<< bits << endl;
-
-    BS -> writeNbits(NBITS,bits);
-
-    BS ->writeOnfile( argv[argc-2]);
+    BS ->writeOnfile( argv[argc-3]);
 
     return 0;
 }
