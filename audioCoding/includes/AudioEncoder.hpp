43b16df7fa35c9c0618acc6d97ad73fd2306a35b
Bruno Pereira <babp5315@gmail.com>
2020-11-28 14:20:49 +0000

    audio coding initial program structure

---
+++ audioCoding/includes/AudioEncoder.hpp
@@ -0,0 +1,38 @@
+//
+// Created by bruno on 11/27/20.
+//
+
+#ifndef AUDIOCODING_AUDIOENCODER_H
+#define AUDIOCODING_AUDIOENCODER_H
+
+#include    <sndfile.hh>
+#include    <vector>
+
+
+class AudioEncoder {
+private:
+    SndfileHandle sourceFile;
+    std::vector<short> rightChRes;
+    std::vector<short> leftChRes;
+
+public:
+    /**
+     * Constructor
+     * @param filename
+     */
+    AudioEncoder(char* filename);
+
+    /**
+     * Computes residuals for each channel using predictor.
+     */
+    void encode();
+
+    /**
+     * Use BitStream to write Golomb encoded residuals to file.
+     * @param filename
+     */
+    void write(char* filename);
+};
+
+
+#endif //AUDIOCODING_AUDIOENCODER_H
---
+++ audioCoding/src/AudioEncoder.cpp
@@ -0,0 +1,5 @@
+//
+// Created by bruno on 11/27/20.
+//
+
+#include "../includes/AudioEncoder.hpp"
