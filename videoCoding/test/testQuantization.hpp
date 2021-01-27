6ff514295a11bc7fec51bd1c0cfb561c0d8e1791
Inês Justo <inesjusto@ua.pt>
2021-01-27 22:50:51 +0000

    inverse dct

--- videoCoding/test/testQuantization.hpp
+++ videoCoding/test/testQuantization.hpp
@@ -113,20 +113,108 @@ TEST_CASE("Applying the DCT to the block") {
     CHECK(std::equal(Y_matrix.begin<double>(), Y_matrix.end<double>(), block.begin<double>()));
 }
 
+TEST_CASE("Applying the inverse of the DCT") {
+    double X [8][8] = {{183,160,94,153,194,163,132,165},
+                       {183,153,116,176,187,166,130,169},
+                       {179,168,171,182,179,170,131,167},
+                       {177,177,179,177,179,165,131,167},
+                       {178,178,179,176,182,164,130,171},
+                       {179,180,180,179,183,169,132,169},
+                       {179,179,180,182,183,170,129,173},
+                       {180,179,181,179,181,170,130,169}};
+    double Y [8][8] = {{313,56,-27,18,78,-60,27,-27},
+                       {-38,-27,13,44,32,-1,-24,-10},
+                       {-20,-17,10,33,21,-6,-16,-9},
+                       {-10,-8,9,17,9,-10,-13,1},
+                       {-6,1,6,4,-3,-7,-5,5},
+                       {2,3,0,-3,-7,-4,0,3},
+                       {4,4,-1,-2,-9,0,2,4},
+                       {3,1,0,-4,-2,-1,3,1}};
+
+    cv::Mat X_matrix = cv::Mat(8, 8, CV_64F, &X);
+    cv::Mat block = cv::Mat(8, 8, CV_64F, &Y);
+    cv::Mat Y_matrix = cv::Mat(8, 8, CV_64F, &Y);
+
+    INFO("\ndct: \n");
+    INFO(Y_matrix);
+    INFO("\nblock: \n");
+    INFO(block);
+
+    inverseDCT(block);
+
+    INFO("\ninverseDCT: \n");
+    INFO(block);
+
+    INFO("\nX: \n");
+    INFO(X_matrix);
+
+    INFO("\nDifferences between X and inverse of DCT: \n");
+    INFO(X_matrix-block);
+
+    bool is_small_diff = cv::checkRange(X_matrix-block, true, nullptr, -2, 2);
+    CHECK(is_small_diff);
+}
+
 TEST_CASE("Applying the quantization matrix") {
+    double Y [8][8] = {{313,56,-27,18,78,-60,27,-27},
+                       {-38,-27,13,44,32,-1,-24,-10},
+                       {-20,-17,10,33,21,-6,-16,-9},
+                       {-10,-8,9,17,9,-10,-13,1},
+                       {-6,1,6,4,-3,-7,-5,5},
+                       {2,3,0,-3,-7,-4,0,3},
+                       {4,4,-1,-2,-9,0,2,4},
+                       {3,1,0,-4,-2,-1,3,1}};
     cv::Mat block = cv::Mat(8, 8, CV_64F, &Y);
     cv::Mat finalY = cv::Mat(8, 8, CV_64F, &final_Y);
 
     INFO("\nY: \n");
     INFO(block);
-    quantDCTCoeff(block, quantMatrixGrayscale);
+    quantDCTCoeff(block, quantMatrixLuminance);
     INFO("\nQ: \n");
-    INFO(quantMatrixGrayscale);
+    INFO(quantMatrixLuminance);
     INFO("\nỸ: \n");
     INFO(finalY);
     INFO("\nresult: \n");
     INFO(block);
     CHECK(std::equal(block.begin<double>(), block.end<double>(), finalY.begin<double>()));
+}
+
+TEST_CASE("Removing the effect of the quantization matrix") {
+
+    double Y [8][8] = {{313,56,-27,18,78,-60,27,-27},
+                       {-38,-27,13,44,32,-1,-24,-10},
+                       {-20,-17,10,33,21,-6,-16,-9},
+                       {-10,-8,9,17,9,-10,-13,1},
+                       {-6,1,6,4,-3,-7,-5,5},
+                       {2,3,0,-3,-7,-4,0,3},
+                       {4,4,-1,-2,-9,0,2,4},
+                       {3,1,0,-4,-2,-1,3,1}};
+    double final_Y [8][8] = {{20,5,-3,1,3,-2,1,0},
+                             {-3,-2,1,2,1,0,0,0},
+                             {-1,-1,1,1,1,0,0,0},
+                             {-1,0,0,1,0,0,0,0},
+                             {0,0,0,0,0,0,0,0},
+                             {0,0,0,0,0,0,0,0},
+                             {0,0,0,0,0,0,0,0},
+                             {0,0,0,0,0,0,0,0}};
+    cv::Mat initialY = cv::Mat(8, 8, CV_64F, &Y);
+    cv::Mat block = cv::Mat(8, 8, CV_64F, &final_Y);
+
+    INFO("\nQ: \n");
+    INFO(quantMatrixLuminance);
+    INFO("\nỸ: \n");
+    INFO(block);
+    inverseQuantDCTCoeff(block, quantMatrixLuminance);
+    INFO("\nY: \n");
+    INFO(initialY);
+    INFO("\nresult: \n");
+    INFO(block);
+
+    INFO("\nDifferences between Y and inverse of the quantization: \n");
+    INFO(initialY-block);
+
+    bool is_small_diff = cv::checkRange(initialY-block, true, nullptr, -30, 30);
+    CHECK(is_small_diff);
 }
 
 TEST_CASE("DCT Quantization From Start To Finish"){
@@ -145,9 +233,9 @@ TEST_CASE("DCT Quantization From Start To Finish"){
 
     INFO("\nX: \n");
     INFO(block);
-    wholeDCTQuant(block, quantMatrixGrayscale);
+    wholeDCTQuant(block, quantMatrixLuminance);
     INFO("\nQ: \n");
-    INFO(quantMatrixGrayscale);
+    INFO(quantMatrixLuminance);
     INFO("\nỸ: \n");
     INFO(finalY);
     INFO("\nresult: \n");
@@ -155,7 +243,52 @@ TEST_CASE("DCT Quantization From Start To Finish"){
     CHECK(std::equal(block.begin<double>(), block.end<double>(), finalY.begin<double>()));
 }
 
+TEST_CASE("DCT Inverse Quantization From Start To Finish"){
+
+    double X [8][8] = {{183,160,94,153,194,163,132,165},
+                       {183,153,116,176,187,166,130,169},
+                       {179,168,171,182,179,170,131,167},
+                       {177,177,179,177,179,165,131,167},
+                       {178,178,179,176,182,164,130,171},
+                       {179,180,180,179,183,169,132,169},
+                       {179,179,180,182,183,170,129,173},
+                       {180,179,181,179,181,170,130,169}};
+
+    double final_Y [8][8] = {{20,5,-3,1,3,-2,1,0},
+                             {-3,-2,1,2,1,0,0,0},
+                             {-1,-1,1,1,1,0,0,0},
+                             {-1,0,0,1,0,0,0,0},
+                             {0,0,0,0,0,0,0,0},
+                             {0,0,0,0,0,0,0,0},
+                             {0,0,0,0,0,0,0,0},
+                             {0,0,0,0,0,0,0,0}};
+    cv::Mat block = cv::Mat(8, 8, CV_64F, &final_Y);
+    cv::Mat initialX = cv::Mat(8, 8, CV_64F, &X);
+
+    INFO("\nỸ: \n");
+    INFO(block);
+    inverseWholeQuant(block, quantMatrixLuminance);
+    INFO("\nQ: \n");
+    INFO(quantMatrixLuminance);
+    INFO("\nX: \n");
+    INFO(initialX);
+    INFO("\nresult: \n");
+    INFO(block);
+    INFO("\nDifferences between X and inverse of the quantization: \n");
+    INFO(initialX-block);
+    bool is_small_diff = cv::checkRange(initialX-block, true, nullptr, -30, 30);
+    CHECK(is_small_diff);
+}
+
 TEST_CASE("Zig Zag San"){
+    double final_Y [8][8] = {{20,5,-3,1,3,-2,1,0},
+                             {-3,-2,1,2,1,0,0,0},
+                             {-1,-1,1,1,1,0,0,0},
+                             {-1,0,0,1,0,0,0,0},
+                             {0,0,0,0,0,0,0,0},
+                             {0,0,0,0,0,0,0,0},
+                             {0,0,0,0,0,0,0,0},
+                             {0,0,0,0,0,0,0,0}};
     cv::Mat block = cv::Mat(8, 8, CV_64F, &final_Y);
     INFO("\nY: \n");
     INFO(block);
